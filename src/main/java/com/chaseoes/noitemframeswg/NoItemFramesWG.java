06ecec820bdce3d9a7e21939183e57798f6808c2
chaseoes <chase.oes@gmail.com>
2014-01-26 19:12:22 -0700

    Add auto updater and metrics.

--- src/main/java/com/chaseoes/noitemframeswg/NoItemFramesWG.java
+++ src/main/java/com/chaseoes/noitemframeswg/NoItemFramesWG.java
@@ -1,11 +1,15 @@
 package com.chaseoes.noitemframeswg;
 
+import java.io.IOException;
 import java.util.List;
+
+import net.gravitydevelopment.updater.Updater;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.Metrics;
 
 public class NoItemFramesWG extends JavaPlugin {
 
@@ -20,6 +24,17 @@ public class NoItemFramesWG extends JavaPlugin {
         getConfig().options().copyDefaults(true);
         saveConfig();
         getServer().getPluginManager().registerEvents(new ItemFrameListeners(), this);
+
+        if (getConfig().getBoolean("allow-auto-updating")) {
+            new Updater(this, 73310, this.getFile(), Updater.UpdateType.DEFAULT, false);
+        }
+
+        try {
+            Metrics metrics = new Metrics(this);
+            metrics.start();
+        } catch (IOException e) {
+            // Failed to submit the stats :-(
+        }
     }
 
     public void onDisable() {
@@ -31,7 +46,7 @@ public class NoItemFramesWG extends JavaPlugin {
             cs.sendMessage(ChatColor.YELLOW + getDescription().getName() + ChatColor.GRAY + " version " + getDescription().getVersion() + ChatColor.GRAY + " by " + ChatColor.YELLOW + getDescription().getAuthors().get(0) + ChatColor.GRAY + ".");
             return true;
         }
-        
+
         if (!cs.hasPermission("noitemframeswg.admin")) {
             cs.sendMessage(ChatColor.RED + "You don't have permission.");
             return true;
@@ -52,7 +67,7 @@ public class NoItemFramesWG extends JavaPlugin {
                 cs.sendMessage(ChatColor.RED + "Usage: /" + cmnd.getName() + " add <region name>");
             }
         }
-        
+
         if (strings[0].equalsIgnoreCase("remove")) {
             if (strings.length == 2) {
                 List<String> regions = getRegions();
